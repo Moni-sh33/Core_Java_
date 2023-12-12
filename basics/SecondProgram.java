c81b7e32c944eed9cb26c9e88474a08a754b98db
MONISH <127576740+Moni-sh33@users.noreply.github.com>
2023-12-12 19:51:36 +0530

    Create SecondProgram.java

---
+++ basics/SecondProgram.java
@@ -0,0 +1,17 @@
+package com.tnsif.basics;
+
+/**
+ * @author Sagar
+ *
+ */
+public class SecondProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
