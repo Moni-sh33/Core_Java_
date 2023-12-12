7f992beb7f81e816da8966cc4f646ff5d529e3e7
MONISH <127576740+Moni-sh33@users.noreply.github.com>
2023-12-12 20:50:38 +0530

    Update BankImpl.java

--- interfaces/implementinginterfaces/BankImpl.java
+++ interfaces/implementinginterfaces/BankImpl.java
@@ -1,5 +1,5 @@
 //BankImpl class implementing the Bank interface
-package com.tnsif.interfaces.implementinginterfaces;
+package com.interfaces.implementinginterfaces;
 
 public class BankImpl implements Bank {
  @Override
